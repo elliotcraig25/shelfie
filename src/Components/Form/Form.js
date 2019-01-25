9cbea3ae874b3072cee4e6cc6d95b90d48d03875
elliot <elliotgcraig@icloud.com>
2019-01-25 11:20:51 -0700

    working on input and buttons step 1

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -3,9 +3,50 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
+    constructor(){
+        super()
+        this.state = {
+            imageUrlInput: '',
+            productNameInput: '',
+            priceInput: ''
+        }
+    }
+
+    handleImageUrlInput(value){
+        this.setState({
+            imageUrlInput: value
+        })
+        console.log(this.state.imageUrlInput)
+    }
+
+    handleProductNameInput(value){
+        this.setState({
+            productNameInput: value
+        })
+        console.log(this.state.productNameInput)
+    }
+
+    handlePriceInput(value){
+        this.setState({
+            priceInput: value
+        })
+        console.log(this.state.priceInput)
+    }
+
+    handleCancelClick(){
+        this.setState({
+            imageUrlInput: '',
+            productNameInput: '',
+            priceInput: ''
+        })
+        console.log(this.state.imageUrlInput)
+        console.log(this.state.productNameInput)
+        console.log(this.state.priceInput)       
+    }
+
     render(){
         return(
-            <div>
+            <div className="parent-form">
                 <div className="form-image">
                     <div className="form-image-div">
 
@@ -13,14 +54,23 @@ class Form extends Component {
                 </div>
                 <div className="form-inputs">
                     <p>Image URL:</p>
-                    <input />
+                    <input onChange={(e)=>{
+                        this.handleImageUrlInput(e.target.value)
+                    }}/>
                     <p>Product Name:</p>
-                    <input />
+                    <input onChange={(e)=>{
+                        this.handleProductNameInput(e.target.value)
+                    }}/>
                     <p>Price:</p>
-                    <input />
+                    <input onChange={(e)=>{
+                        this.handlePriceInput(e.target.value)
+                    }}/>
                 </div>
                 <div className="form-buttons">
-
+                    <button onClick={()=>{
+                        this.handleCancelClick()
+                    }}>Cancel</button>
+                    <button>Add to inventory</button>
                 </div>
             </div>
         )
