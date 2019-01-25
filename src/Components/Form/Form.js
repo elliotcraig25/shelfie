755a300be5352abc13f525c68d2bd63669aba241
elliot <elliotgcraig@icloud.com>
2019-01-25 11:31:24 -0700

    input cancel button is working

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -41,7 +41,7 @@ class Form extends Component {
         })
         console.log(this.state.imageUrlInput)
         console.log(this.state.productNameInput)
-        console.log(this.state.priceInput)       
+        console.log(this.state.priceInput)      
     }
 
     render(){
@@ -56,15 +56,15 @@ class Form extends Component {
                     <p>Image URL:</p>
                     <input onChange={(e)=>{
                         this.handleImageUrlInput(e.target.value)
-                    }}/>
+                    }} value={this.state.imageUrlInput}/>
                     <p>Product Name:</p>
                     <input onChange={(e)=>{
                         this.handleProductNameInput(e.target.value)
-                    }}/>
+                    }} value={this.state.productNameInput}/>
                     <p>Price:</p>
                     <input onChange={(e)=>{
                         this.handlePriceInput(e.target.value)
-                    }}/>
+                    }} value={this.state.priceInput}/>
                 </div>
                 <div className="form-buttons">
                     <button onClick={()=>{
